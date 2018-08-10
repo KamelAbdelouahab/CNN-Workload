5731c37743c2b851c2eadc852534397f7e5c6c6a
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-08-10 09:55:01 +0200

    Added support of Scale layers

--- Topology.py
+++ Topology.py
@@ -20,20 +20,20 @@ def Topology(protoFile, modelFile):
     cnn     = caffe.Net(protoFile,1,weights=modelFile)
     params  = cnn.params
     blobs   = cnn.blobs
-    
+
     for l in cnn._layer_names:
         layerId = list(cnn._layer_names).index(l)
         layerType =  cnn.layers[layerId].type
 
         if (layerType == 'Convolution'):
-            non_lin = ""        
+            non_lin = ""
             N = params[l][0].data.shape[0]
             C = params[l][0].data.shape[1]
             J = params[l][0].data.shape[2]
             K = params[l][0].data.shape[3]
             U = blobs[l].data.shape[2]
             V = blobs[l].data.shape[3]
-            
+
             for i in range (1,6):
                 try:
                     cnn.layers[layerId+i]
@@ -41,6 +41,8 @@ def Topology(protoFile, modelFile):
                         break
                     if (cnn.layers[layerId+i].type == 'ReLU'):
                         non_lin = non_lin + "+" + "ReLU"
+                    if (cnn.layers[layerId+i].type == 'Scale'):
+                        non_lin = non_lin + "+" + "Scale"
                     if (cnn.layers[layerId+i].type == 'TanH'):
                         non_lin = non_lin + "+" + "TanH"
                     if (cnn.layers[layerId+i].type == 'Sigmoid'):
@@ -48,34 +50,36 @@ def Topology(protoFile, modelFile):
                     if (cnn.layers[layerId+i].type == 'Pooling'):
                         non_lin = non_lin + "+" + "Pool"
                     if (cnn.layers[layerId+i].type == 'LRN' or cnn.layers[layerId+i].type == "BatchNorm"):
-                        non_lin = non_lin + "+" + "BN" 					
+                        non_lin = non_lin + "+" + "BN"
                 except IndexError:
-                    pass				
-		
-            print(l      + "\t" + 
-                  str(C) + "\t" + 
-                  str(N) + "\t" +  
-                  str(J) + "x" +  
-                  str(K) + "\t" +  
-                  str(U) + "x" +  
-                  str(V) + "\t" + 
+                    pass
+
+            print(l      + "\t" +
+                  str(C) + "\t" +
+                  str(N) + "\t" +
+                  str(J) + "x" +
+                  str(K) + "\t" +
+                  str(U) + "x" +
+                  str(V) + "\t" +
                   "" + non_lin + "");
         if (layerType == 'InnerProduct'):
-            non_lin = ""        
+            non_lin = ""
             N = params[l][0].data.shape[0]
             C = params[l][0].data.shape[1]
             for i in range (4):
                 try:
                     cnn.layers[layerId+i]
+                    if (cnn.layers[layerId+i].type == 'TanH'):
+                        non_lin = non_lin + "+" + "TanH"                    
                     if (cnn.layers[layerId+i].type == 'ReLU'):
                         non_lin = non_lin + "+" + "ReLU"
                     if (cnn.layers[layerId+i].type == 'Softmax'):
-                        non_lin = non_lin + "+" + "Softmax"    					
+                        non_lin = non_lin + "+" + "Softmax"
                 except IndexError:
-                    pass                
-            print(l      + "\t" + 
+                    pass
+            print(l      + "\t" +
                   str(C) + "\t" +
-                  str(N) + "\t" + "\t" +  "\t" + 
+                  str(N) + "\t" + "\t" +  "\t" +
                   "" + non_lin + "");
 
 if __name__ == '__main__':
